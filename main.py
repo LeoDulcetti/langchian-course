6efc1be6e21d96d068d0e39898e5c523a9a41fe9
Leonardo Dulcetti <leonardodulcetti@dhcp-9-241-239-12.01s-es.ibmmobiledemo.com>
2025-10-30 11:20:29 +0100

    settings

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Tavily helps the agent to connect to the Web API. It has a Free plan. Tavily is gonna expose the services and APIs to the agents.
+# It is a langchain provider.
